8815ef0bf92a6adc015973c1d2010260ff3d5ab5
Yuichi Ogino <yuichi.ogino@sansan.com>
2023-03-23 11:40:27 +0900

    initial

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      nativeWindowOpen: true,
+    },
+  });
+
+  win.loadURL('https://chat.openai.com/chat');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
